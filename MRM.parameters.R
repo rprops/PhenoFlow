5ceead97e70384a63b73a596529e14ecc844f90c
rprops <Ruben.Props@UGent.be>
2016-08-03 11:49:02 -0400

    Update MRM.parameters.R

--- MRM.parameters.R
+++ MRM.parameters.R
@@ -197,8 +197,12 @@ beta.div.fcm <- function(x, d=3, n=3, dist="bray",k=2,iter=100,ord.type=c("NMDS"
 
 plot.beta.fcm <- function(x, color=NA,shape=NA,labels=c("Factor 1","Factor 2"),legend.pres=NULL){
   require('ggplot2')
-  if (sum(is.na(color))>=1) color=rep("f1",nrow(x$points))
-  if (sum(is.na(shape))>=1) shape=rep("f2",nrow(x$points))
+  legend.ops<-NULL
+  if (sum(is.na(color))>0) color=rep("f1",nrow(x$points))
+  if (sum(is.na(shape))>0) {
+    shape=rep("f2",nrow(x$points))
+    legend.ops <- FALSE
+    }
   var.pcoa <- eigenvals(x)/sum(eigenvals(x))
   PcoA <- as.data.frame(x$points)
   names(PcoA)[1:2] <- c('Axis1', 'Axis2')
@@ -210,7 +214,7 @@ plot.beta.fcm <- function(x, color=NA,shape=NA,labels=c("Factor 1","Factor 2"),l
     labs(x=paste0("Axis1 (",round(100*var.pcoa[1],1),"%)"),y=paste0("Axis2 (",round(100*var.pcoa[2],1),"%)"))+
     ggtitle("PCoA of phenotypic fingerprints")+
     labs(color=labels[1],shape=labels[2])+
-    guides(color=legend.pres, shape=legend.pres)
+    guides(color=legend.pres, shape=legend.ops)
 }
 
 dist.fcm <- function(x, d=3, n=3, dist="bray"){
